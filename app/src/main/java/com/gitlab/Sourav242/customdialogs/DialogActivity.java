e155b38558c16e1a1f225a232491e7eb569cce4b
Sourav242 <1994sourav@gmail.com>
2019-07-30 15:08:13 +0530

    Cleaned App with implementation fileTree(include: ['*.aar'], dir: 'libs') and include ':app'

---
+++ app/src/main/java/com/gitlab/Sourav242/customdialogs/DialogActivity.java
@@ -0,0 +1,15 @@
+package com.gitlab.Sourav242.customdialogs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DialogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialog);
+        //new NormalDialog(this).setTitle("Hello").setMessage("Hello message").show();
+    }
+}
